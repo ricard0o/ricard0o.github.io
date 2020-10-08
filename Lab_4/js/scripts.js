21e99be3807d89255fa5f6e2db1b1d6f99382cb7
ricard0o <71363034+ricard0o@users.noreply.github.com>
2020-10-08 00:21:34 -0400

    Update scripts.js

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,6 +1,7 @@
 let myFullName = "Bill Farmer"; // 10 characters in name...not including space
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
-
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
 
