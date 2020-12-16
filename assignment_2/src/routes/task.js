8637fa3f83888438c1ebe9296a2f290990eb30e4
ricard0o <71363034+ricard0o@users.noreply.github.com>
2020-12-16 00:48:17 -0500

    updated

--- assignment_2/src/routes/task.js
+++ assignment_2/src/routes/task.js
@@ -136,7 +136,7 @@ router.delete('/task', (req, res) => {
 //GET tasks
 router.get('/allTasks', (req, res) => {
 
-    let sql = "get taskName from tasks";
+    let sql = "Select taskName from tasks";
     db.all(sql, (err, row) => {
       if (err) {
         res.status(400).json({ "error": err.message });
